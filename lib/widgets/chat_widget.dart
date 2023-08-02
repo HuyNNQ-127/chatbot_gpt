b263e1c9608791b8ce442b011e1ccb098b2bdabc
HuyNNQ-127 <ngocquanghuy020@gmail.com>
2023-08-02 16:47:42 +0700

    Add Text-to-speech plugin

--- lib/widgets/chat_widget.dart
+++ lib/widgets/chat_widget.dart
@@ -1,9 +1,13 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:chatbot_gpt/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:chatbot_gpt/models/chat_message.dart';
+//import 'package:text_to_speech/text_to_speech.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class ChatWidget extends StatelessWidget {
-  const ChatWidget({
+  ChatWidget({
     super.key,
     required this.message,
     required this.chatIndex,
@@ -11,6 +15,17 @@ class ChatWidget extends StatelessWidget {
 
   final String message;
   final int chatIndex;
+  FlutterTts tts = FlutterTts();
+  bool _speaking = false;
+
+  void chat_speaking() async {
+    _speaking = !_speaking;
+    if (_speaking) {
+      await tts.speak(message);
+    } else {
+      tts.stop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +45,11 @@ class ChatWidget extends StatelessWidget {
                   width: 40,
                   height: 40,
                 ),
-                Expanded(child: TextWidget(label: message))
+                Expanded(child: TextWidget(label: message)),
+                if (chatIndex == 1)
+                  IconButton(
+                      onPressed: chat_speaking,
+                      icon: const Icon(Icons.volume_up_rounded))
               ],
             ),
           ),
